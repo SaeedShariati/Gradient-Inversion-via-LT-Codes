f7b7d049e7ce40d8e7eb4f9938505efc453b3a8c
SaeedShariati <global.arrive2017@gmail.com>
2026-09-11 12:23:07 +0330

    changed comment

--- peeling_attack.py
+++ peeling_attack.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Trap-weight gradient inversion with iterative sample subtraction
+peeling attack gradient inversion
 """
 import os
 os.environ.setdefault('TF_CPP_MIN_LOG_LEVEL', '3')
